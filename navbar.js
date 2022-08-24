38566cd864e3185e6cc9c17fdf71409068f626a1
jeevan aloor <jeevanaloor500@gmail.com>
2022-08-24 19:24:17 +0530

    add tripoto project

---
+++ navbar.js
@@ -0,0 +1,29 @@
+
+
+function navbar(){
+    return `<div id="navbar">
+    <div>
+        <img src="https://cdn1.tripoto.com/assets/2.9/img/logo/tripoto.svg" alt="">
+      
+    
+    </div>
+    <div>
+        <h3>Inspirations</h3>
+    </div>
+    <div>
+        <h3>forum</h3>
+    </div>
+    <div>
+        <h3>packages</h3>
+    </div>
+    <div>
+        <h3>publish trip</h3>
+    </div>
+    <div>
+        <h3>signin</h3>
+    </div>
+    
+    
+    </div>`
+}
+export default navbar
