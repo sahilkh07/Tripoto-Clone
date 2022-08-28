556200776ce9b95a19568fadf7648f34f55c80c2
Mohammad Sahil <starlordz956@gmail.com>
2022-08-28 17:52:57 +0530

    added update

---
+++ index/1.js
@@ -0,0 +1,8 @@
+let doc = document.querySelector('.parts').addEventListener('click',()=>{
+     
+      window.location.href="/mindful/mindful.html"
+})
+
+document.querySelector('.item').addEventListener('click',()=>{
+window.location.href="/content page/content.html"
+})
